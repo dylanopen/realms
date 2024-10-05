47ef4ba0432f9d74236843dd295c213eb5b28655
Dylan Rogers <opendylan@proton.me>
2024-10-05 18:03:37 +0100

    document Vec2f

--- src/component.rs
+++ src/component.rs
@@ -1,30 +1,88 @@
 use crate::Color;
 
+/// `Vec2f` is a struct for storing an `x` and `y`
+/// component, both stored as a 32-bit float (`f32`).
+///
+/// It is commonly used for coordinates (positions) and
+/// dimensions (sizes).
 pub struct Vec2f {
     pub x: f32,
     pub y: f32,
 }
 
 impl Vec2f {
+    /// Create a new `Vec2f` with the specified `x` and `y`
+    /// fields.
+    ///
+    /// ## Example usage
+    ///
+    /// ```rust
+    /// let rect_position = Vec2f::new(100.0, 80.0);
+    /// let rect_size = Vec2f::new(200.0, 160.0);
+    /// ```
     pub fn new(x: f32, y: f32) -> Vec2f {
         Vec2f {x, y}
     }
 
+    /// Increments `self.x` by `other.x`, as well as
+    /// incrementing `self.y` by `other.y`.
+    ///
+    /// ## Example usage
+    ///
+    /// ```rust
+    /// let mut a = Vec2f::new(20.0, 10.0);
+    /// let b = Vec2f::new(5.0, 4.0);
+    /// a.add(b);
+    /// // `a` is now (25.0, 14.0)
+    /// ```
     pub fn add(&mut self, other: &Vec2f) {
         self.x += other.x;
         self.y += other.y;
     }
 
+    /// Decrements `self.x` by `other.x`, as well as
+    /// decrementing `self.y` by `other.y`.
+    ///
+    /// ## Example usage
+    ///
+    /// ```rust
+    /// let mut a = Vec2f::new(20.0, 10.0);
+    /// let b = Vec2f::new(5.0, 4.0);
+    /// a.sub(b);
+    /// // `a` is now (15.0, 6.0)
+    /// ```
     pub fn sub(&mut self, other: &Vec2f) {
         self.x -= other.x;
         self.y -= other.y;
     }
 
+    /// Multiplies `self.x` by `other.x`, as well as
+    /// multiplying `self.y` by `other.y`.
+    ///
+    /// ## Example usage
+    ///
+    /// ```rust
+    /// let mut a = Vec2f::new(20.0, 10.0);
+    /// let b = Vec2f::new(5.0, 4.0);
+    /// a.mul(b);
+    /// // `a` is now (100.0, 40.0)
+    /// ```
     pub fn mul(&mut self, other: &Vec2f) {
         self.x *= other.x;
         self.y *= other.y;
     }
 
+    /// Divides `self.x` by `other.x`, as well as
+    /// dividing `self.y` by `other.y`.
+    ///
+    /// ## Example usage
+    ///
+    /// ```rust
+    /// let mut a = Vec2f::new(20.0, 10.0);
+    /// let b = Vec2f::new(5.0, 4.0);
+    /// a.div(b);
+    /// // `a` is now (4.0, 2.5)
+    /// ```
     pub fn div(&mut self, other: &Vec2f) {
         self.x /= other.x;
         self.y /= other.y;
