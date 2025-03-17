dfc1b67942033fd2428deee3009669eba77b3f6e
Dylan Rogers <opendylan@proton.me>
2025-03-17 07:29:59 +0000

    add lints to warn on missing doctests

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,11 @@
-//! Welcome to Realms!
-//! Realms is a lightweight, simple and powerful library which provides graphics and game functionality.
+//! Welcome to Realms!  
+//! Realms is a lightweight, simple and powerful library which provides graphics and game functionality.  
 //! 
-//! Homepage: <https://github.com/dylanopen/realms>
-//! Documentation: <https://docs.rs/realms>
+//! Homepage: <https://github.com/dylanopen/realms>  
+//! Documentation: <https://docs.rs/realms>  
 
+#![warn(missing_docs)]
+#![warn(clippy::missing_docs_in_private_items)]
 
 pub mod window;
 pub mod input;
