206c9c948dad64144b82e25ccf47905aa56f4944
Dylan Rogers <opendylan@proton.me>
2025-03-17 16:59:30 +0000

    clippy: allow similar field names, pub use and non-alphabetical field ordering

--- src/lib.rs
+++ src/lib.rs
@@ -6,6 +6,10 @@
 
 #![warn(missing_docs)]
 
+#[allow(clippy::similar_names)]
+#[allow(clippy::pub_use)]
+#[allow(clippy::arbitrary_source_item_ordering)]
+
 pub mod window;
 pub mod input;
 pub mod shader;
