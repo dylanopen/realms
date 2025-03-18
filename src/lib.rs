deeeb227ea77040167b1d575ce3a272f06426b31
Dylan Rogers <opendylan@proton.me>
2025-03-18 17:24:34 +0000

    disable all additional clippy lints

--- src/lib.rs
+++ src/lib.rs
@@ -4,12 +4,6 @@
 //! Homepage: <https://github.com/dylanopen/realms>  
 //! Documentation: <https://docs.rs/realms>  
 
-#![warn(missing_docs)]
-
-#[allow(clippy::similar_names)]
-#[allow(clippy::pub_use)]
-#[allow(clippy::arbitrary_source_item_ordering)]
-
 pub mod window;
 pub mod input;
 pub mod shader;
@@ -19,4 +13,19 @@ pub mod texture;
 
 pub use gl;
 pub use glfw;
+
+
+// LINTS
+// =====
+// Realms intentionally uses many clippy lints in order to ensure code stays
+// as readable and maintainable as possible.
+// While it is always preferred to actually *fix* the lints that Clippy
+// detects, if a lint makes adding / updating a feature very difficult, you
+// are welcome to sprinkle `#[expect(...)]` throughout your code if you
+// absolutely need to.
+//
+// If you believe a lint should be removed from Realms, please make an issue
+// or PR explaining why it should be removed.
+
+
 
