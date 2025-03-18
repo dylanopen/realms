7f4b27a5fdef24b1115c4cfc03f8b555e31376fe
Dylan Rogers <opendylan@proton.me>
2025-03-18 18:40:19 +0000

    split library info in lib.rs into two lines

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
 //! Welcome to Realms!  
-//! Realms is a lightweight, simple and powerful library which provides graphics and game functionality.  
+//! Realms is a lightweight, simple and powerful library which provides
+//! graphics and game functionality.  
 //! 
 //! Homepage: <https://github.com/dylanopen/realms>  
 //! Documentation: <https://docs.rs/realms>  
@@ -17,6 +18,7 @@
 // If you believe a lint should be removed from Realms, please make an issue
 // or PR explaining why it should be removed.
 
+#![warn(missing_docs)]
 #![warn(clippy::absolute_paths)]
 #![warn(clippy::allow_attributes)]
 #![warn(clippy::allow_attributes_without_reason)]
